a25810eae24412dd82fbf0aaad5041c920a6ef57
shreyascr <shrs601@gmail.com>
2019-08-30 16:37:08 +0530

    [shreyas|sneha] Add.Calulatig area with one parameter as zero and another as nonzero

--- src/test/java/com/thoughtworks/com/bootcamp/RectangleTest.java
+++ src/test/java/com/thoughtworks/com/bootcamp/RectangleTest.java
@@ -1,13 +1,23 @@
 package com.thoughtworks.com.bootcamp;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class RectangleTest {
-    void ToCalculateAreaOfRectangleAsLengthZeroAndBreadthZero_WhenArea_ThenReturnZero()
+    @Test
+    void ToCalculateAreaOfRectangleAsLengthZeroAndBreadthZero_WhenArea_ThenReturnZero() {
+        float length = 0.0f;
+        float breadth = 0.0f;
+        Rectangle rectangle = new Rectangle(length, breadth);
+        Assertions.assertEquals(0.0f, rectangle.area());
+    }
+
+    @Test
+    void ToCalculateAreaOfRectangleAsOneOfItsValueMustBeZero_WhenAreaCalculated_ThenReturnZero()
     {
-        float length=0.0f;
-        float breadth=0.0f;
-        Rectangle rectangle=new Rectangle(length,breadth);
-        Assertions.assertEquals(0.0f,rectangle.area());
+        float length = 10.0f;
+        float breadth = 0.0f;
+        Rectangle rectangle = new Rectangle(length, breadth);
+        Assertions.assertEquals(0.0f, rectangle.area());
     }
 }
